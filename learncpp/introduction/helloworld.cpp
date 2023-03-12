5b4cae74b542732693beacd50146873cf24967d8
BOZKURT <emrebzkurt1@gmail.com>
2023-03-12 15:24:35 +0300

    FIX: added some comments

--- learncpp/introduction/helloworld.cpp
+++ learncpp/introduction/helloworld.cpp
@@ -1,8 +1,11 @@
-
+// The first line indicates preprocessor directive. The preprocessor directive indicates that we would like to use
+// the contents of the iostream library, which is a part of the C++ standard library that helps us read and write text
+// into the command line.
 #include <iostream>
 
 int main()
 {
+    // std::cout stands for "character output" and << operator allows us to send letters or numbers to the console.
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
