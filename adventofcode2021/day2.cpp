6e9eca3a1f9c0855aedcb2067e821c320f0d87ee
BOZKURT <emrebzkurt1@gmail.com>
2023-04-09 13:34:03 +0300

    ADD: day2 fixed for second question

--- adventofcode2021/day2.cpp
+++ adventofcode2021/day2.cpp
@@ -3,27 +3,30 @@
 #include <string>
 #include <vector>
 
+
 int position_calculator(std::string file_name){
     std::ifstream input(file_name);
     std::string line;
     std::string forward = "forward";
     std::string depth_down = "down";
     std::string depth_up = "up";
+    int aim = 0;
     int depth = 0;
     int horizontal_position = 0;
 
     while(std::getline(input, line)){
         if(line.find(forward) != std::string::npos){
             int forward_length = std::stoi(line.substr(8, 1));
+            depth += aim * forward_length;
             horizontal_position += forward_length;
         }
         else if(line.find(depth_down) != std::string::npos){
             int depth_length = std::stoi(line.substr(5, 1));
-            depth += depth_length;
+            aim += depth_length;
         }
         else if(line.find(depth_up) != std::string::npos){
             int depth_length = std::stoi(line.substr(3, 1));
-            depth -= depth_length;
+            aim -= depth_length;
         }
     }
     return horizontal_position * depth;
