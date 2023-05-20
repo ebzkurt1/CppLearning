5980cecab60fb86fb98f705b58b874474fa40dec
BOZKURT <emrebzkurt1@gmail.com>
2023-05-20 15:13:57 +0300

    ADD: new challange, finished first two steps

---
+++ coding_challange/challange_day_20_05_23.cpp
@@ -0,0 +1,43 @@
+// The main idea of this challange is to build uniq cli tool.
+// 20.05.23
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(int argc, char* argv[])
+    // argc - number of arguments represents number of command line arguments passed to the program
+    // char* argv[] - This parameter is an array of strings (char*) representing the command-line arguments.
+    // argv stands for "argument vector." 
+{
+    // read input file line by line
+
+    for (int i = 1; i < argc; i++)
+    {
+        std::string filename = argv[i];
+        std::ifstream inFile(filename);
+
+        if (!inFile)
+        {
+            std::cout << "Error opening file: " << filename << "\n";
+            return 1;
+        }
+
+        std::string line;
+        std::string pre;  // previous line 
+        bool is_first_line = true;
+        while(std::getline(inFile, line))
+        {
+            if(is_first_line || line != pre)
+            {
+                std::cout << line << "\n";
+                is_first_line = false;
+            }
+            pre = line;
+        }
+        inFile.close();
+
+    }
+    return 0;
+}
