a2b41c4b9534bcece2b6c6ad6502cecf25aa35c8
BOZKURT <emrebzkurt1@gmail.com>
2023-05-24 23:30:19 +0300

    ADD: functionality to use two options and finished the challange

--- coding_challange/challange_day_20_05_23.cpp
+++ coding_challange/challange_day_20_05_23.cpp
@@ -5,48 +5,104 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <vector>
 
 
-void read_standard_input(std::string filename, bool write_to_output = false, std::string output_filename = "")
+std::map<std::string, int> process_input_file(std::string filename)
+    // read input file line by line and save it to an ordered map
 {
-    if (write_to_output)
+    std::map<std::string, int> input_lines;
+    std::ifstream inFile(filename);
+    std::string line;
+    if (!inFile)
     {
-        std::ofstream out_file(output_filename);
-        std::string line;
-        std::string pre;  // previous line
-        bool is_first_line = true;
-        while(std::getline(std::cin, line))
-        {
-            if(is_first_line)
-            {
-                out_file << line;
-                is_first_line = false;
-            }
-            else if(line != pre)
-            {
-                out_file << '\n' << line;
-            }
-            pre = line;
-        }
-        out_file.close();
+        std::cout << "Error opening file: " << filename << "\n";
+        return input_lines;
     }
-    else
+    while(std::getline(inFile, line))
     {
-        std::string line;
-        std::string pre;  // previous line
-        bool is_first_line = true;
+        ++input_lines[line];
+    }
+    inFile.close();
+    return input_lines;
+}
+
+
+void read_from_standard_input(int argc, char* argv[])
+    // read from standard input and print it to standard output
+{
+    std::string line;
+    std::string pre;
+    bool first_line = true;
+    if(argc == 2)
+    {
         while(std::getline(std::cin, line))
         {
-            if(is_first_line)
+            if(first_line)
             {
                 std::cout << line;
-                is_first_line = false;
+                first_line = false;
             }
             else if(line != pre)
             {
                 std::cout << '\n' << line;
             }
             pre = line;
+        }
+    }
+    else if(argc == 3)
+    {
+        std::ofstream outFile(argv[2]);
+        while(std::getline(std::cin, line))
+        {
+            if(first_line)
+            {
+                outFile << line;
+                first_line = false;
+            }
+            else if(line != pre)
+            {
+                outFile << '\n' << line;
+            }
+            pre = line;
+        }
+    }
+}
+
+
+void out_only_unique(std::map<std::string, int> input_lines, bool print_count = false)
+{
+    for(const auto& pair : input_lines)
+    {
+        if(pair.second == 1)
+        {
+            if(print_count)
+            {
+                std::cout << pair.second << " " << pair.first << '\n';
+            }
+            else 
+            {
+                std::cout << pair.first << '\n';
+            }
+        }
+    }
+}
+
+
+void out_only_repeat(std::map<std::string, int> input_lines, bool print_count = false)
+{
+    for(const auto& pair : input_lines)
+    {
+        if(pair.second > 1)
+        {
+            if(print_count)
+            {
+                std::cout << pair.second << " " << pair.first << '\n';
+            }
+            else 
+            {
+                std::cout << pair.first << '\n';
+            }
         }
     }
 }
@@ -57,171 +113,53 @@ int main(int argc, char* argv[])
     // char* argv[] - This parameter is an array of strings (char*) representing the command-line arguments.
     // argv stands for "argument vector." 
 {
-    // read input file line by line
-
-    for (int i = 1; i < argc; i++)
+    std::vector<std::string> user_inputs;
+    for(int i = 1; i < argc; i++)
     {
-        std::cout << argv[i] << "\n";
-        if (argv[i] == "-")
-        {
-            if (argc >= 3)
-            {
-            }
-        }
+        std::string user_input = argv[i];
+        user_inputs.push_back(user_input);
     }
-
-    std::string arg1 = argv[1];
-    if (arg1 == "-")
+    if(argc < 2)
     {
-        // if an output file is specified, write to it
-        // else read from standard input
-        if (argc >= 3)
-        {
-            std::string filename = argv[2];
-            std::ofstream out_file(filename);
-            std::string line;
-            std::string pre;  // previous line
-            bool is_first_line = true;
+        std::cout << "Error: no arguments passed\n";
+        return 1;
     }
-    else if (arg1 == "-c")
+    else if(user_inputs[0] == "-")
     {
-        if (argc >= 3)
-        {
-            std::string filename = argv[2];
-            std::ifstream inFile(filename);
-
-            if (!inFile)
-            {
-                std::cout << "Error opening file: " << filename << "\n";
-                return 1;
-            }
-
-            std::string line;
-            std::string pre;  // previous line 
-            int count = 1;
-            bool is_first_line = true;
-            while(std::getline(inFile, line))
-            {
-                if(is_first_line)
-                {
-                    is_first_line = false;
-                }
-                else if(line != pre)
-                {
-                    std::cout << '\n' << count << " " << pre;
-                    count = 1;
-                }
-                else
-                {
-                    count++;
-                }
-                pre = line;
-            }
-            std::cout << '\n' << count << " " << pre;
-            inFile.close();
-        }
+        read_from_standard_input(argc, argv);
     }
-
-    else if (arg1 == "-d")
-    {
-        if (argc >= 3)
-        {
-            std::string filename = argv[2];
-            std::ifstream inFile(filename);
-
-            if (!inFile)
-            {
-                std::cout << "Error opening file: " << filename << "\n";
-                return 1;
-            }
-
-            std::string line;
-            std::string pre;  // previous line
-            bool written_before = false;
-            bool is_first_line = true;
-            while(std::getline(inFile, line))
-            {
-                if(is_first_line)
-                {
-                    is_first_line = false;
-                }
-                else if(line == pre)
-                {
-                    std::cout << '\n' << line;
-                    written_before = true;
-                }
-                else
-                {
-                    written_before = false;
-                }
-                pre = line;
-            }
-        }
-    }
-
-    else if (arg1 == "-u")
-    {
-        if (argc >= 3)
-        {
-            std::string filename = argv[2];
-            std::ifstream inFile(filename);
-
-            if (!inFile)
-            {
-                std::cout << "Error opening file: " << filename << "\n";
-                return 1;
-            }
-
-            std::map<std::string, int> input_lines;
-            std::string line;
-            
-            while(std::getline(inFile, line))
-            {
-                ++input_lines[line];
-            }
-            inFile.close();
-
-            for (const auto& pair : input_lines)
-            {
-                if(pair.second == 1)
-                {
-                    std::cout << pair.first << '\n';
-                }
-            }
-        }
-    }
-
     else
     {
-        for (int i = 1; i < argc; i++)
+        std::string filename = argv[argc - 1];
+        std::map<std::string, int> input_lines = process_input_file(filename);
+        if(argc > 3)
         {
-            std::string filename = argv[i];
-            std::ifstream inFile(filename);
-
-            if (!inFile)
+            if(user_inputs[0] == "-d" || user_inputs[0] == "--repeated" || user_inputs[1] == "-d" || user_inputs[1] == "--repeated" )
             {
-                std::cout << "Error opening file: " << filename << "\n";
-                return 1;
+                out_only_repeat(input_lines, user_inputs[0] == "-c" || user_inputs[1] == "-c");
             }
-
-            std::string line;
-            std::string pre;  // previous line 
-            bool is_first_line = true;
-            while(std::getline(inFile, line))
+            else if(user_inputs[0] == "-u" || user_inputs[1] == "-u")
             {
-                if(is_first_line)
-                {
-                    std::cout << line;
-                    is_first_line = false;
-                }
-                else if(line != pre)
-                {
-                    std::cout << '\n' << line;
-                }
-                pre = line;
+                out_only_unique(input_lines, user_inputs[0] == "-c" || user_inputs[1] == "-c");
             }
-            inFile.close();
-
+        }
+        else if(argc <=3)
+        {
+            if(user_inputs[0] == "-d" || user_inputs[0] == "--repeated")
+            {
+                out_only_repeat(input_lines);
+            }
+            else if(user_inputs[0] == "-u")
+            {
+                out_only_unique(input_lines);
+            }
+            else if(user_inputs[0] == "-c")
+            {
+                for(const auto& pair : input_lines)
+                {
+                    std::cout << pair.second << " " << pair.first << '\n';
+                }
+            }
         }
     }
     return 0;
