6f162df0e7fd907fe38bc13570bd21ec6035f130
BOZKURT <emrebzkurt1@gmail.com>
2023-05-24 23:47:02 +0300

    ADD: attempts at the challange

---
+++ coding_challange/challange_day_23_05_23.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <vector>
+
+
+std::vector<std::vector<int>> generate_square_matrix(int n)
+{
+    std::vector<std::vector<int>> matrix(n, std::vector<int>(n, 0));
+    int i = 0;
+    int j = 0;
+}
+
+int main()
+{
+    std::cout << "Please provide a positive integer:\n";
+    std::cin >> n;
+
+
+    return 0;
+}
