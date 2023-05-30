453b14a0e809102ebcf6ddcc7838f5644c5750f7
BOZKURT <emrebzkurt1@gmail.com>
2023-05-30 23:42:50 +0300

    ADD: solution for step 1; listen port 80 in local host

--- coding_challange/challange_day_27_05_23.cpp
+++ coding_challange/challange_day_27_05_23.cpp
@@ -3,6 +3,10 @@
 
 
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
 #define PORT 80
 
@@ -18,9 +22,40 @@ int main(int argc, char* argv[])
         std::cout << "Error creating socket" << '\n';
         return -1;
     }
+    
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(PORT);
+    if (bind(local_socket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to bind socket." << std::endl;
+        close(local_socket);
+        return 1;
+    }
 
+    if (listen(local_socket, 5) < 0)
+    {
+        std::cout << "Error listening to socket" << '\n';
+        close(local_socket);
+        return -1;
+    }
 
+    std::cout << "Listening to port " << PORT << '\n';
 
+    sockaddr_in clientAddress{ };
+    socklen_t clientAddressSize = sizeof(clientAddress);
+    int clientSocket = accept(local_socket, (struct sockaddr*) &clientAddress, &clientAddressSize);
+    if (clientSocket < 0)
+    {
+        std::cout << "Error accepting client" << '\n';
+        close(local_socket);
+        return -1;
+    }
+
+    std::cout << "Client connected" << '\n';
+
+    close(clientSocket);
+    close(local_socket);
 
     return 0;
 }
