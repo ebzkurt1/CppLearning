b3cbea5c10535e2946aefbd5eb06eb35a0b001e4
BOZKURT <emrebzkurt1@gmail.com>
2023-05-30 22:49:35 +0300

    ADD: steps for next steps of the challange

--- coding_challange/challange_day_27_05_23.cpp
+++ coding_challange/challange_day_27_05_23.cpp
@@ -4,10 +4,23 @@
 
 #include <iostream>
 
+#define PORT 80
 
 
-
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    // program a simple socket server that listens to port 80 on localhost
+    // and returns a simple HTTP response
+
+    int local_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (local_socket < 0)
+    {
+        std::cout << "Error creating socket" << '\n';
+        return -1;
+    }
+
+
+
+
+    return 0;
 }
